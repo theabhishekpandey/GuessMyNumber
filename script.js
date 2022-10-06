f810f57676eefe18410b1bea92247057e549365b
theabhishekpandey <105118511+theabhishekpandey@users.noreply.github.com>
2022-10-06 22:01:24 +0530

    Add files via upload
    Initial Commit

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+let secretNumber=Math.trunc(Math.random()*20)+1;
+let score=20;
+let highscore=0;
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+  };
+function checkOnClick()
+{
+    const guess=Number(document.querySelector('.guess').value);
+    if(!guess)
+    {
+        displayMessage('⛔No number');
+    }
+    else if(guess===secretNumber)
+    {
+        displayMessage('✔ Correct number');
+        document.querySelector('.number').textContent=guess;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+          }
+    }
+    else if(guess!=secretNumber)
+    {
+        if(score>1)
+        {
+            displayMessage(guess>secretNumber?'Too high': 'Too low');
+            score--;
+            document.querySelector('.score').textContent=score;
+        }
+        else
+        {
+            displayMessage('You lost the game');
+            document.querySelector('.score').textContent='0';
+        }
+    }
+}
+document.querySelector('.check').addEventListener('click',checkOnClick);
+function againOnClick()
+{
+   score=20;
+   highscore=0;
+   secretNumber = Math.trunc(Math.random() * 20) + 1;
+   displayMessage('Start guessing...');
+   document.querySelector('.score').textContent=score;
+   document.querySelector('.number').textContent = '?';
+   document.querySelector('.guess').value = '';
+   document.querySelector('body').style.backgroundColor = '#222';
+   document.querySelector('.number').style.width = '15rem';
+}
+document.querySelector('.again').addEventListener('click',againOnClick);
